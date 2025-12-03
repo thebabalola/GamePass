bc06c9f1887882a89f64fdc17fd922a76d64c113
dev.uche <uchennahanson@gmail.com>
2025-12-03 18:25:13 +0100

    feat: add contract declaration and documentation

--- contracts/src/GamePassToken.sol
+++ contracts/src/GamePassToken.sol
@@ -7,3 +7,18 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
+/**
+ * @title GamePassToken
+ * @dev ERC20 token contract for GamePass platform
+ * Compatible with Celo network
+ * 
+ * Features:
+ * - Standard ERC20 functionality
+ * - Burnable tokens
+ * - Pausable transfers
+ * - Maximum supply of 1 billion tokens
+ * - Minting capabilities for rewards and swap contracts
+ * - Treasury receives 50% of supply on deployment
+ */
+contract GamePassToken is ERC20, ERC20Burnable, ERC20Pausable, Ownable, ReentrancyGuard {
+
