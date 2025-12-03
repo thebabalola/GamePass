3dcd6d08ac3d6152929c01f2ee8c635f88a5b15e
ogazboiz <ogazboizakpolo@gmail.com>
2025-12-03 15:28:17 +0100

    feat: add contract declaration and inheritance

--- contracts/src/GamePassGem.sol
+++ contracts/src/GamePassGem.sol
@@ -8,3 +8,12 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
+/**
+ * @title GamePassGem
+ * @dev ERC721 NFT contract for GamePass play-to-earn game
+ * Compatible with Celo network
+ * NFTs are referred to as "GEMS" in the game
+ */
+contract GamePassGem is ERC721URIStorage, Ownable, ReentrancyGuard {
+    using SafeERC20 for IERC20;
+
