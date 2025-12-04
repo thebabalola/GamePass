3086931350020ddfaca467b3e3e94e43019f3c91
dev.uche <uchennahanson@gmail.com>
2025-12-04 12:37:19 +0100

    test: create GamePassSwap test file structure

---
+++ contracts/test/GamePassSwap.t.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {Test, console} from "forge-std/Test.sol";
+import {GamePassSwap} from "../src/GamePassSwap.sol";
+import {GamePassToken} from "../src/GamePassToken.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
