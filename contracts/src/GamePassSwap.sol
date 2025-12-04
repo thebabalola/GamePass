7ddc9c91415fcbcee3b6b5ef7f62025b41c81899
dev.uche <uchennahanson@gmail.com>
2025-12-04 13:24:24 +0100

    feat: add contract declaration and documentation

--- contracts/src/GamePassSwap.sol
+++ contracts/src/GamePassSwap.sol
@@ -7,3 +7,17 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./GamePassToken.sol";
 
+/**
+ * @title GamePassSwap
+ * @dev Contract for swapping CELO and cUSD for PASS tokens
+ * Compatible with Celo network
+ * 
+ * Features:
+ * - Buy PASS tokens with CELO (native currency)
+ * - Buy PASS tokens with cUSD (stablecoin)
+ * - Configurable exchange rates
+ * - Direct token minting to buyers
+ * - Owner-controlled rate updates
+ */
+contract GamePassSwap is Ownable, ReentrancyGuard {
+
